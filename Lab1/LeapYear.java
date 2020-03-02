d207cd1b7715bd65f73f99240547adf035138a9d
Yifan <yifanluo33@gmail.com>
2020-03-02 11:26:16 +0800

    LeapYear uploaded

--- Lab1/LeapYear.java
+++ Lab1/LeapYear.java
@@ -1,11 +1,15 @@
 /** Class that determines whether or not a year is a leap year.
- *  @author YOUR NAME HERE
+ *  @author Yifan
  */
 public class LeapYear {
 
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
      */
+    public static boolean isLeapYear(int year){
+        return ((year % 4 == 0) || (year % 400 == 0) && (year % 100 != 0));
+    }
+
     private static void checkLeapYear(int year) {
         if (isLeapYear(year)) {
             System.out.printf("%d is a leap year.\n", year);
---
+++ hw0/hw0_exercise_1b.java
@@ -0,0 +1,4 @@
+package PACKAGE_NAME;
+
+public class hw0_exercise_1b {
+}
