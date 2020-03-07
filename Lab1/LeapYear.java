a0de1aefa2f37967fecc5eaa128737ac4663800e
Yifan <yifanluo33@gmail.com>
2020-03-08 00:18:55 +0800

    Completed Lab1

--- Lab1/LeapYear.java
+++ Lab1/LeapYear.java
@@ -6,7 +6,7 @@ public class LeapYear {
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
      */
-    public static boolean isLeapYear(int year){
+    public static boolean isLeapYear(int year) {
         return ((year % 4 == 0) || (year % 400 == 0) && (year % 100 != 0));
     }
 
