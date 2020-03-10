0e6ace1030774af074cfd419032a933c8378e0e0
Yifan <yifanluo33@gmail.com>
2020-03-10 17:51:40 +0800

    Proj0 Completed

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -1,3 +1,5 @@
+import examples.StdDraw;
+
 public class NBody {
 /** This class is aim to simulate the law of Universal Graviation*/
     public static double readRadius(String filename) {
@@ -8,29 +10,80 @@ public class NBody {
     }
 
     public static Planet[] readPlanets(String filename) {
-        if (filename.isEmpty()) {
-            System.out.println("Please supply a country as a command line argument.");
-            System.out.println("For countries with spaces, use an underscore, e.g. South_Korea");
-			/* NOTE: Please don't use System.exit() in your code.
-			   It will break the autograder. */
-//            System.exit(0);
-        }
-
         In in2 = new In(filename);
         int number2 = in2.readInt();
         Planet[] allstars = new Planet[number2];
-        double radius_uni2 = in2.readDouble();
+        in2.readDouble();
         int count = 0;
         while (count < number2) {
-            allstars[count].xxPos = in2.readDouble();
-            allstars[count].yyPos = in2.readDouble();
-            allstars[count].xxVel = in2.readDouble();
-            allstars[count].yyVel = in2.readDouble();
-            allstars[count].mass = in2.readDouble();
-            allstars[count].imgFileName = in2.readString();
+            double xP = in2.readDouble();
+            double yP = in2.readDouble();
+            double xV = in2.readDouble();
+            double yV = in2.readDouble();
+            double mass = in2.readDouble();
+            String name = in2.readString();
+            allstars[count] = new Planet(xP, yP, xV, yV, mass, name);
             count++;
         }
         return allstars;
+    }
+
+    public static void main(String[] args) {
+        // Read the args[] and planets file
+        double T = Double.valueOf(args[0]);
+        double dt = Double.valueOf(args[1]);
+        String filename = args[2];
+        double uniRadius = readRadius(filename);
+        Planet[] stars = readPlanets(filename);
+//        System.out.println(uniRadius);
+//        System.out.println(stars[0].mass);
+
+        // Draw the background of universe
+        StdDraw.setScale(-uniRadius, uniRadius);
+//        StdDraw.setScale(-100,100);
+        StdDraw.clear();
+        String imageName = "images/starfield.jpg";
+        StdDraw.picture(0,0, imageName);
+        StdDraw.show();
+        StdDraw.pause(2000);
+//        int count = 0;
+        int number = stars.length;
+        for (int count = 0; count < number; count++) {
+            Planet.draw(stars[count]);
+        }
+
+        //Enable double buffering
+        StdDraw.enableDoubleBuffering();
+
+        for (double timer = 0; timer < T; timer += dt) {
+            double[] xForces = new double[number];
+            double[] yForces = new double[number];
+//            StdDraw.setScale(-uniRadius, uniRadius);
+//            StdDraw.clear();
+//            StdDraw.picture(0,0, imageName);
+//            StdDraw.show();
+            for (int count = 0; count < number; count++) {
+                xForces[count] = stars[count].calcNetForceExertedByX(stars);
+                yForces[count] = stars[count].calcNetForceExertedByY(stars);
+                stars[count].update(dt, xForces[count], yForces[count]);
+                Planet.draw(stars[count]);
+
+//                StdDraw.pause(1000);
+//                StdDraw.picture(stars[count].xxPos, stars[count].yyPos, stars[count].imgFileName);
+            }
+
+
+
+        }
+
+        StdOut.printf("%d\n", stars.length);
+        StdOut.printf("%.2e\n", uniRadius);
+        for (int i = 0; i < stars.length; i++) {
+            StdOut.printf("%11.4e %11.4e %11.4e %11.4e %11.4e %12s\n",
+                    stars[i].xxPos, stars[i].yyPos, stars[i].xxVel,
+                    stars[i].yyVel, stars[i].mass, stars[i].imgFileName);
+        }
+
     }
 
 }
