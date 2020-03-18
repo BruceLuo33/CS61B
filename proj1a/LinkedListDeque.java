fc0f79b14d4c4b5d46eb69adbad48724b034cd71
Yifan <yifanluo33@gmail.com>
2020-03-18 16:34:18 +0800

    LinkedListDeque Started

---
+++ proj1a/LinkedListDeque.java
@@ -0,0 +1,41 @@
+public class LinkedListDeque<T> {
+    private int item;
+    private int size;
+    private T sentinel;
+
+
+    public void addFirst(T item) {
+
+    }
+
+    public void addLast(T item) {
+
+    }
+
+    public boolean isEmpty() {
+
+    }
+
+    public int size() {
+
+    }
+
+    public void printDeque() {
+
+    }
+
+    public T removeFirst() {
+
+    }
+
+    public T removeLast() {
+
+    }
+
+    public T get(int index) {
+
+    }
+
+
+
+}
