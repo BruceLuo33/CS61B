22f5a4a75c7ae14caade092c1e0fc8df6d8d62ea
Yifan <yifanluo33@gmail.com>
2020-03-26 18:30:21 +0800

    Deque

--- proj1b/Deque.java
+++ proj1b/Deque.java
@@ -1,3 +1,26 @@
-public interface Deque {
+public interface Deque<T> {
+
+
+    public void addFirst(T item);
+
+    public void addLast(T item);
+
+    public T removeFirst();
+
+    public T removeLast();
+
+    public boolean isEmpty();
+
+    public int size();
+
+    public T get(int index);
+
+    public void printDeque();
+
+    /* The getRecursive method only implemented in the LinkedListDeque*/
+//    public T getRecursive(int index);
+
+
+
 
 }
