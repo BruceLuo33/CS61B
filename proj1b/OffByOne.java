dcb8ed7c5fcea14604bda7597a86ce31742ae3cf
Yifan <yifanluo33@gmail.com>
2020-03-30 17:05:23 +0800

    Proj1b Completed

--- proj1b/OffByOne.java
+++ proj1b/OffByOne.java
@@ -1,2 +1,10 @@
-public class OffByOne {
+public class OffByOne implements CharacterComparator {
+
+    @Override
+    public boolean equalChars(char x, char y) {
+        if (x - y == 1 || x - y == -1) {
+            return true;
+        }
+        return false;
+    }
 }
