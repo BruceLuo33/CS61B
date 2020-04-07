ace4fbe6e41d42cd0beb067b90fa5726f8b5a961
Yifan <yifanluo33@gmail.com>
2020-04-07 15:12:19 +0800

    Lab6 Completed

--- lab6/TestUnionFind.java
+++ lab6/TestUnionFind.java
@@ -1,2 +1,55 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class TestUnionFind {
+
+//    @Test
+    public void testBasic() {
+        UnionFind uf = new UnionFind(10);
+        //Test size
+        int expected = 1;
+        int actural = uf.sizeOf(3);
+        assertEquals(expected, actural);
+
+        //Test parent method
+        expected = -1;
+        actural = uf.parent(2);
+        assertEquals(expected, actural);
+
+        // Test connect method
+        assertFalse(uf.connected(2,3));
+
+        // Test find method
+        expected = 2;
+        actural = uf.find(2);
+        assertEquals(expected, actural);
+
+    }
+
+    @Test
+    public void testUnion() {
+        UnionFind uf = new UnionFind(10);
+
+        // Union the disjointed sets with one layer
+        uf.union(4,3);
+        int expected = 4;
+        int actural = uf.find(3);
+        assertEquals(expected, actural);
+
+        uf.union(3,8);
+        actural = uf.find(8);
+        assertEquals(expected,actural);
+
+        uf.union(6,5);
+        uf.union(9,4);
+        uf.union(8,9);
+        int expectedParent = 4;
+        int acturalParent = uf.parent(8);
+        assertEquals(expectedParent, acturalParent);
+
+
+
+
+    }
+
 }
--- lab6/UnionFind.java
+++ lab6/UnionFind.java
@@ -1,8 +1,14 @@
+import javax.swing.*;
+
 public class UnionFind {
 
+    /**Implement the Weighted Quick Union Algorithm.
+     * We need to connect the relative smaller tree to the bigger one.
+     * To achieve this, we need to add an array in it to store the vertices numbers.*/
     // TODO - Add instance variables?
+    private int[] parent;
     private int[] id;
-    private int size;
+    private int[] size;
     private int count;
 
     /* Creates a UnionFind data structure holding n vertices. Initially, all
@@ -10,8 +16,12 @@ public class UnionFind {
     public UnionFind(int n) {
         // TODO
         id = new int[n];
+        size = new int[n];
+        parent = new int[n];
         for (int i = 0; i < n; i++) {
             id[i] = i;
+            parent[i] = -1;
+            size[i] = 1;
         }
         count = n;
     }
@@ -29,20 +39,30 @@ public class UnionFind {
     /* Returns the size of the set v1 belongs to. */
     public int sizeOf(int v1) {
         // TODO
-        return -1;
+        int sz = 1;
+        while (parent[v1] >= 0) {
+            v1 = parent[v1];
+            sz += 1;
+        }
+        return sz;
     }
 
     /* Returns the parent of v1. If v1 is the root of a tree, returns the
        negative size of the tree for which v1 is the root. */
     public int parent(int v1) {
         // TODO
-        return -1;
+//        if (parent[v1] < 0) {
+//            return parent[v1];
+//        } else {
+//            return parent[v1];
+//        }
+        return parent[v1];
     }
 
     /* Returns true if nodes v1 and v2 are connected. */
     public boolean connected(int v1, int v2) {
         // TODO
-        return parent(v1) == parent(v2);
+        return find(v1) == find(v2);
     }
 
     /* Connects two elements v1 and v2 together. v1 and v2 can be any valid 
@@ -52,13 +72,30 @@ public class UnionFind {
        change the sets but may alter the internal structure of the data. */
     public void union(int v1, int v2) {
         // TODO
+        int v1Root = find(v1);
+        int v2Root = find(v2);
+        int v1Size = sizeOf(v1);
+        int v2Size = sizeOf(v2);
+        if (v1Root != v2Root) {
+            if (v1Size >= v2Size) {
+                parent[v2Root] = v1Root;
+                parent[v1Root] = -(v1Size + v2Size);
+            } else {
+                parent[v1Root] = v2Root;
+                parent[v2Root] = -(v1Size + v2Size);
+            }
+        }
+
     }
 
     /* Returns the root of the set V belongs to. Path-compression is employed
        allowing for fast search-time. */
     public int find(int vertex) {
         // TODO
-        return -1;
+        while (vertex != parent[vertex] && parent[vertex] >= 0) {
+            vertex = parent[vertex];
+        }
+        return vertex;
     }
 
 }
